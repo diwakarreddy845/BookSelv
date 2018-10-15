59fb9eab037afcfa1ef47ebc5bfeb232cf528d40
Diwakar Reddy Gudisi <diwakarreddy45@gmail.com>
2018-10-15 10:57:15 +0530

    angular material is addedd

--- src/main/ui/src/app/app.module.ts
+++ src/main/ui/src/app/app.module.ts
@@ -1,16 +1,83 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatAutocompleteModule, MatBadgeModule, MatBottomSheetModule, MatButtonModule,
+  MatButtonToggleModule, MatCardModule, MatCheckboxModule, MatChipsModule,
+  MatDatepickerModule, MatDialogModule, MatDividerModule, MatExpansionModule,
+  MatGridListModule, MatIconModule, MatInputModule, MatListModule, MatMenuModule,
+  MatNativeDateModule, MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule,
+  MatRadioModule, MatRippleModule, MatSelectModule, MatSidenavModule, MatSliderModule, MatSlideToggleModule,
+  MatSnackBarModule, MatSortModule, MatStepperModule, MatTableModule, MatTabsModule,
+  MatToolbarModule, MatTooltipModule, MatTreeModule
+} from '@angular/material';
+import { LoginComponent } from './login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { RegistrationComponent } from './registration/registration.component';
+import { UseraccountComponent } from './useraccount/useraccount.component';
+import { BookdetailsComponent } from './bookdetails/bookdetails.component';
+import { UserComponent } from './user/user.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
+
+const appRoutes: Routes = [
+  // { path: '', component: LandingpageComponent },
+  // { path: 'login', component: LoginComponent },
+  // { path: 'registration', component: RegistrationComponent },
+  { path: 'useraccount', component: UseraccountComponent },
+  {
+    path: 'signup', component: UserComponent,
+    children: [{ path: '', component: SignUpComponent }]
+  },
+  {
+    path: 'login', component: UserComponent,
+    children: [{ path: '', component: SignInComponent }]
+  },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LandingpageComponent,
+    LoginComponent,
+    RegistrationComponent,
+    UseraccountComponent,
+    BookdetailsComponent,
+    UserComponent,
+    SignInComponent,
+    SignUpComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule, ReactiveFormsModule, BrowserAnimationsModule,
+    MatAutocompleteModule, MatBadgeModule, MatBottomSheetModule, MatButtonModule,
+    MatButtonToggleModule, MatCardModule, MatCheckboxModule, MatChipsModule,
+    MatStepperModule, MatDatepickerModule, MatDialogModule, MatDividerModule,
+    MatExpansionModule, MatGridListModule, MatIconModule, MatInputModule,
+    MatListModule, MatMenuModule, MatNativeDateModule, MatPaginatorModule,
+    MatProgressBarModule, MatProgressSpinnerModule, MatRadioModule, MatRippleModule,
+    MatSelectModule, MatSidenavModule, MatSliderModule, MatSlideToggleModule,
+    MatSnackBarModule, MatSortModule, MatTableModule, MatTabsModule, MatToolbarModule,
+    MatTooltipModule, MatTreeModule, FormsModule,
+
+    RouterModule.forRoot(appRoutes)
   ],
+  entryComponents: [
+    BookdetailsComponent
+  ],
+
   providers: [],
   bootstrap: [AppComponent]
+
 })
-export class AppModule { }
+export class AppModule {
+
+}
